7355333dafc37e8a8dd59a80f8a120130fd93456
GreenAsJade <greenasjade@gregories.net>
2022-07-13 15:24:11 +0930

    Missing file

---
+++ src/Pages/Action/Action.tsx
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2022 Martin Gregory.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import "./Page1.css";
+
+import * as React from "react";
+
+import { FontAwesomeIcon as FA } from "@fortawesome/react-fontawesome";
+import { faDiceD6, faDiceD20 } from "@fortawesome/free-solid-svg-icons";
+
+import * as CharacterTypes from "CharacterTypes";
+
+type ActionProps = {
+    character: CharacterTypes.CharacterType;
+    setStat: (name: string, value: number) => void;
+};
+
+export const Action = (props: ActionProps): JSX.Element => {
+    const rollFor = (name: string, range: CharacterTypes.StatRange) => {
+        const newValue = Math.floor(Math.random() * range) + 1;
+        props.setStat(name, newValue);
+    };
+
+    console.log(props.character);
+    return (
+        <div id="action-page">
+            <div className="character-name">
+                Character: {props.character.name}
+            </div>
+            <div className="stat-config-section">
+                {Object.keys(props.character.stats).length ? (
+                    Object.entries(props.character.stats).map(
+                        ([stat_name, stat]) => (
+                            <div key={stat_name} className="stat-config">
+                                <span>{stat_name}</span>
+                                {stat.value ? (
+                                    <span>{stat.value}</span>
+                                ) : (
+                                    <FA
+                                        icon={
+                                            stat.range === 6
+                                                ? faDiceD6
+                                                : faDiceD20
+                                        }
+                                        onClick={() =>
+                                            rollFor(stat_name, stat.range)
+                                        }
+                                    />
+                                )}
+                            </div>
+                        ),
+                    )
+                ) : (
+                    <span>No statz!</span>
+                )}
+            </div>
+        </div>
+    );
+};
